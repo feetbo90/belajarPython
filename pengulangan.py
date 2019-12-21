192ba6b90c30ab4bd4093432ead68ba7b8608fed
mhdiqbalpradipta <mhdiqbalpradipta@gmail.com>
2019-12-21 11:56:29 +0700

    pengulangan

---
+++ pengulangan.py
@@ -0,0 +1,35 @@
+
+
+count = 1
+batas = 10
+while count < batas :
+    print(count)
+    count = count + 1
+
+print("=============================")
+
+count2 = 10
+batas = 1
+while count2 > batas :
+    print(count2)
+    count2 = count2 - 1
+
+list = [1,2,3,4,5,6]
+# slicing
+print(list[1:5])
+print(list[-5:-1])
+
+for item in list :
+    print(item)
+
+# nilai awal ; kondisi ; increment / decrement
+
+# nilai : 1
+# 1 < 10
+# nilai = nilai - 1
+#
+# nilai = 10
+#
+# 1
+# ...
+# 9
